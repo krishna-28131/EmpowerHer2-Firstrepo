f0d786b478b5e420a68d928102499907590b4de8
krishna-28131 <krishnapurohit417400@gmail.com>
2025-11-20 14:47:48 +0530

    Nested and Object

---
+++ Modern Js-ES6+Feature/Q11/Q12/Q13/Q14/index.js
@@ -0,0 +1,30 @@
+
+let profile = {
+  name: "Charlie",
+  age: 29,
+  address: {
+    city: "San Francisco",
+    zipcode: "94101"
+  }
+};
+
+let updates = {
+  age: 30,
+  address: {
+    zipcode: "94109",
+    country: "USA"
+  }
+};
+
+let mergedProfile = {
+  ...profile,
+  ...updates,
+  address: {
+    ...profile.address,
+    ...updates.address
+  }
+};
+
+console.log(mergedProfile);
+
+
