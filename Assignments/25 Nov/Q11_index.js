f077b75fe242d01b6b6a3393cddc264e271e5c99
krishna-28131 <krishnapurohit417400@gmail.com>
2025-11-27 17:04:34 +0530

    Understanding and Navigating the DOM

---
+++ Assignments/25 Nov/Q11_index.js
@@ -0,0 +1,12 @@
+
+let heading = document.getElementById("mainHeading");
+heading.textContent = "Welcome to the DOM World!";
+
+let paragraphs = document.getElementsByTagName("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "blue";
+}
+
+let containerDiv = document.querySelector(".container");
+containerDiv.style.backgroundColor = "yellow";
